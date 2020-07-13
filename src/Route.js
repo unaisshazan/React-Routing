23294866192a4a05a8463b009d13d2a52415c282
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-14 00:51:08 +0500

    add .

--- src/Route.js
+++ src/Route.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Home from './Components/Home';
+import About from './Components/About';
+import Product from './Components/Product';
+import ProductItem from './Components/ProductItem';
 import {
     BrowserRouter as Router,
     Switch,
