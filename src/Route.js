1e1c8fa00eca6e60cd7c56e168cc5191602a30ca
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-14 00:23:00 +0500

    add .

---
+++ src/Route.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
+function RouteConfig() {
+  return (
+    <div >
+   <Router>
+   <switch>
+   
+   </switch>
+   </Router>
+    </div>
+  );
+}
+
+export default RouteConfig;
