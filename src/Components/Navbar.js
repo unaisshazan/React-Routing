ea1e6e56876711c23a6f40ed24ad007dde7439a8
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-14 01:26:19 +0500

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -17,7 +17,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+
 import Button from '@material-ui/core/Button';
 
 
